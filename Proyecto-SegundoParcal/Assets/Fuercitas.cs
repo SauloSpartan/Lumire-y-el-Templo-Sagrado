fe903714350defd1c44e556af056960b8a7ab62b
SauloTrujillo <SauloSpartan2@gmail.com>
2021-04-10 14:06:24 -0500

    Proyecto con Assets

---
+++ Proyecto-SegundoParcal/Assets/Fuercitas.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Fuercitas : MonoBehaviour
+{
+    void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(10f, 0f, 0f, ForceMode.Impulse);
+    }
+}
