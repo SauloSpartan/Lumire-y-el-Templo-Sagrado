22c72cf8ae4068b5d5d9dfb97ed892cd519edd27
JesusTapia27 <jesusg.tapia.d@gmail.com>
2021-04-14 20:51:32 -0500

    Daños y Vida
    Sistema de daño y vida del personaje terminado

---
+++ Proyecto-SegundoParcal/Assets/Personaje/Codigos/ManipuladorVida.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManipuladorVida : MonoBehaviour
+{
+    VidaPlayer playerVida;
+
+    public int cantidad;
+    public float damgeTime;
+    float currentDamageTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerVida = GameObject.FindWithTag("Player").GetComponent<VidaPlayer>();
+        
+    }
+
+    private void OnTriggerStay (Collider other)
+    {
+        currentDamageTime += Time.deltaTime;
+        if (currentDamageTime > damgeTime)
+        {
+            playerVida.vida += cantidad;
+            currentDamageTime = 0.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Proyecto-SegundoParcal/Assets/Personaje/Codigos/VidaPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VidaPlayer : MonoBehaviour
+{
+    public float vida = 100;
+
+    public Image barraDeVida;
+
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vida = Mathf.Clamp(vida, 0, 100);
+
+        barraDeVida.fillAmount = vida / 100;
+
+        if (vida <= 0)
+        {
+            anim.enabled = false;
+            
+        }
+    }
+}
