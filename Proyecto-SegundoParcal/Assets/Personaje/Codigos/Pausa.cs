a09684dcdf147302e166266d3843dbb7e7f590b3
JesusTapia27 <jesusg.tapia.d@gmail.com>
2021-04-15 15:32:49 -0500

    Player con Pausa
    Le agregue el Menu de Pausa al Player

---
+++ Proyecto-SegundoParcal/Assets/Personaje/Codigos/Pausa.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausa : MonoBehaviour
+{
+    private bool pause = false;
+    public GameObject panelPausa;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause = !pause;
+        }
+
+        if (pause)
+        {
+            Time.timeScale = 0;
+            panelPausa.SetActive(true);
+        }
+
+        if (!pause)
+        {
+            Time.timeScale = 1;
+            panelPausa.SetActive(false);
+        }
+        
+    }
+}
