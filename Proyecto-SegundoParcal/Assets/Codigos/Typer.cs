79dc538736b91ff95ddd4916f6a21e42bd1b2f99
SauloTrujillo <SauloSpartan2@gmail.com>
2021-04-21 01:16:18 -0500

    Escena Cinematica
    Ya empecé a hacer la escena de la cinematica implementando el activado y desactivado de componentes con música y botones

---
+++ Proyecto-SegundoParcal/Assets/Codigos/Typer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Typer : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+}
